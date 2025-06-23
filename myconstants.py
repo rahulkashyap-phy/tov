9f3718ef94002075971101277fbb3c586a0c6a0b
Rahul Kashyap <rahulkashyap411@gmail.com>
2025-06-23 17:52:59 +0530

    correction in myconstants and plotting in MeV/fm^3

--- myconstants.py
+++ myconstants.py
@@ -7,7 +7,7 @@ Sources: Google, Wikipedia, NIST
 Jeff Kaplan <jeffkaplan@caltech.edu>
 February 2, 2013
 """
- 
+
 #################################
 # Physical constants
 #
@@ -17,7 +17,7 @@ February 2, 2013
 # H - Plank's constant
 # HBAR - Plank's constant/2pi
 # K  -  Boltzmann constant
- 
+
 #################################
 # Astronomical constants
 #
@@ -30,8 +30,8 @@ February 2, 2013
 # AU  - Astronomical unit
 # PC  - Parsec
 # LYR - Light year
- 
- 
+
+
 ##===================================================================
 ## Meters kilograms seconds (SI)
 ##===================================================================
@@ -41,7 +41,7 @@ MKS_AMU = 1.660538921e-27 # kg
 MKS_H = 6.62606957e-34     # J s = m^2 kg / s
 MKS_HBAR = 1.054571726e-34 # J s = m^2 kg / s
 MKS_K = 1.3806488e-23  # J K^-1
- 
+
 MKS_MSUN = 1.98855e30  # kg
 MKS_RSUN = 6.955e8     # m
 MKS_LSUN = 3.933e26    # W -Watt
@@ -51,7 +51,7 @@ MKS_R_EARTH = 6478100.0 # m
 MKS_AU   = 1.49597871e11 # m
 MKS_PC   = 3.08567758e16  # m
 MKS_LYR  = 9.4605284e15  # M
- 
+
 #################################
 # Conversion constants
 #
@@ -69,7 +69,7 @@ MKS_ERG  = 1.0e-7      # J
 MKS_DYNE = 1.0e-5      # N = m  kg s^-2
 MKS_GRAM_PER_CM_CUBED = 1000.0 # kg m ^-3
 MKS_DYNE_PER_CM_SQUARED = 0.1 # Pa = N m^-2  = kg m^-1 s^-2
- 
+
 ##===================================================================
 ## Centimeters grams seconds
 ##===================================================================
@@ -79,7 +79,7 @@ CGS_AMU = 1.660538921e-24 # g
 CGS_H = 6.62606957e-27     # erg s = cm^2 g / s
 CGS_HBAR = 1.054571726e-27 # erg s = cm^2 g / s
 CGS_K = 1.3806488e-16  # erg K^-1
- 
+
 CGS_MSUN = 1.98855e33  # g
 CGS_RSUN = 6.955e10     # cm
 CGS_LSUN = 3.933e33    # erg s^-1
@@ -102,14 +102,14 @@ mp = 1.6726231e-24 # g
 # N - Newton: mks force unit
 # KG_PER_M_CUBED: density conversion to mks
 # N_PER_M_SQUARED: pressure conversion to mks
-CGS_EV = 1.60217646-12 # erg
+CGS_EV = 1.60217646e-12 # erg
 CGS_M = 100.0          # cn
 CGS_KG = 1000.0       # g
 CGS_J  = 1.0e+7      # erg
 CGS_N = 1.0e+5      # dyne = cm  g s^-2
 CGS_KG_PER_M_CUBED = 0.001 # g cm ^-3
 CGS_N_PER_M_SQUARED = 10.0 # ba = dyne cm^-2  = g cm^-1 s^-2
- 
+
 ##===================================================================
 ## Augmented geometric units G=c=Msun=1
 #  Units are dimensionless, except Boltzmann K which has Kelvin^-1
@@ -119,11 +119,11 @@ AGEO_G = 1.0
 AGEO_C = 1.0
 AGEO_AMU = 8.3505012e-58
 AGEO_K  = 7.7229851e-71   # K^-1
- 
+
 AGEO_MSUN = 1.0
 AGEO_RSUN = 470934.0
 AGEO_LSUN = 4.4659113e-16
- 
+
 #################################
 # Conversion constants
 AGEO_EV = 8.9621524e-67
@@ -140,7 +140,7 @@ AGEO_J = 5.59373614e-48
 AGEO_ERG = 5.59373614e-55
 AGEO_W  = 1.13549741e-42
 AGEO_ERGS_PER_S = 1.13549741e-49
- 
+
 #################################
 # Inverse conversion constants
 # Gives you a feel for what '1' is for quantities in AGEO units
